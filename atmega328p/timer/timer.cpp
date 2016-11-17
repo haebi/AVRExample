2fb3619b0014c7eef0ca32db3c0d09fcd3dd8536
haebi <haebi>
2016-11-18 00:27:44 +0900

    add comment timer.cpp

--- atmega328p/timer/timer.cpp
+++ atmega328p/timer/timer.cpp
@@ -11,10 +11,21 @@ int count = 0;
 // Main Function
 int main(void)
 {
-	OCR1A = 0x3D08;
+	// OCR1A =  OCR1AH + OCR1AL = (8 + 8 = 16 bit)
+	// Dec 15624 (?) // 15624 x 1024 = 15998976
+	// Reason for THE CPU CLOCK MUST DIVIDED BY 8 ( 8 means 8 bit ? )
+	OCR1A = 0x3D08; 
 
+	// Page 172
+	// CTC Mode : Set to TOP OCR1A
+	// 0 1 0 0 (TCCR1B:4,  TCCR1B:3, TCCR1A:1, TCCR1A:0)
 	setbit(TCCR1B, WGM12);
+
 	setbit(TIMSK1, OCIE1A);
+
+	// Page 173
+	// Clock Select Bit ( CS12 CS11 CS10 )
+	// TCCR1B CS12, CS10 set to 1 means : clock I/O / 1024 (From prescaler)
 	setbit(TCCR1B, CS12);
 	setbit(TCCR1B, CS10);
 
